07fb4cd6db523d7d57d71791e95b4e3a0e43e2be
爱学啊开发 <ixueadev@163.com>
2022-08-25 00:04:32 +0800

    6847 Add image detail.

--- code/video/KanMeiTu/app/src/main/java/com/ixuea/courses/kanmeitu/activity/ImageDetailActivity.java
+++ code/video/KanMeiTu/app/src/main/java/com/ixuea/courses/kanmeitu/activity/ImageDetailActivity.java
@@ -3,8 +3,12 @@ package com.ixuea.courses.kanmeitu.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.ixuea.courses.kanmeitu.R;
+import com.ixuea.courses.kanmeitu.util.Constant;
+import com.ixuea.courses.kanmeitu.util.ImageUtil;
 
 public class ImageDetailActivity extends AppCompatActivity {
 
@@ -12,5 +16,12 @@ public class ImageDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_detail);
+        ImageView imageView = findViewById(R.id.image);
+
+        //获取传递的参数
+        String uri = getIntent().getStringExtra(Constant.ID);
+
+        ImageUtil.show(imageView, uri);
+
     }
 }
