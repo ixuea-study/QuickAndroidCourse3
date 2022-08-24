71475d693396d6e89485d1c9846a4e9e87194b3d
爱学啊开发 <ixueadev@163.com>
2022-08-24 22:23:46 +0800

    6831 Add splash.

---
+++ code/video/KanMeiTu/app/src/main/java/com/ixuea/courses/kanmeitu/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.ixuea.courses.kanmeitu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ixuea.courses.kanmeitu.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ code/video/KanMeiTu/app/src/main/java/com/ixuea/courses/kanmeitu/activity/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.ixuea.courses.kanmeitu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.ixuea.courses.kanmeitu.R;
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
+
+/**
+ * 启动界面
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        QMUIStatusBarHelper.translucent(this);
+
+        //设置状态栏文本颜色为黑色
+        QMUIStatusBarHelper.setStatusBarLightMode(this);
+
+        findViewById(R.id.copyright).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                next();
+            }
+        }, 3000);
+    }
+
+    private void next() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
