c79453615335a3c4c0ef6c769c9f4d5f599c42d7
爱学啊开发 <ixueadev@163.com>
2022-08-24 23:32:40 +0800

    6836 Add image list.

--- code/video/KanMeiTu/app/src/main/java/com/ixuea/courses/kanmeitu/MainActivity.java
+++ code/video/KanMeiTu/app/src/main/java/com/ixuea/courses/kanmeitu/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ixuea.courses.kanmeitu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,7 +10,10 @@ import android.view.View;
 
 import com.ixuea.courses.kanmeitu.activity.BaseActivity;
 import com.ixuea.courses.kanmeitu.activity.LoginActivity;
+import com.ixuea.courses.kanmeitu.adapter.ImageAdapter;
 import com.ixuea.courses.kanmeitu.util.PreferencesUtil;
+
+import java.util.ArrayList;
 
 public class MainActivity extends BaseActivity {
 
@@ -16,8 +21,6 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         //退出登录
         findViewById(R.id.primary).setOnClickListener(new View.OnClickListener() {
@@ -35,5 +38,32 @@ public class MainActivity extends BaseActivity {
                 finish();
             }
         });
+
+        //初始化RecyclerView
+        RecyclerView listView=findViewById(R.id.list);
+        listView.setHasFixedSize(true);
+
+        //显示2列，我们这里实现的是每个图片的宽高都是一样
+        //最好的效果其实是根据图片高度和宽度来缩放
+        //因为每一张图片大小不一样
+        //但这样实现涉及到的知识点很多
+
+        //这里使用了GridLayoutManager
+        //他会显示类似9宫格布局效果
+
+        //详细的可以学习这门课程
+        //http://www.ixuea.com/courses/8
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        listView.setLayoutManager(layoutManager);
+
+        //添加测试数据
+        ArrayList<String> datum = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            //图片来自http://image.baidu.com
+            datum.add(String.format("https://dev-courses-quick.oss-cn-beijing.aliyuncs.com/%d.jpg", i));
+        }
+
+        ImageAdapter adapter = new ImageAdapter(this, datum);
+        listView.setAdapter(adapter);
     }
 }
---
+++ code/video/KanMeiTu/app/src/main/java/com/ixuea/courses/kanmeitu/adapter/ImageAdapter.java
@@ -0,0 +1,60 @@
+package com.ixuea.courses.kanmeitu.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ixuea.courses.kanmeitu.R;
+
+import java.util.List;
+
+/**
+ * 图片adapter
+ */
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+
+    private final Context context;
+    private final List<String> images;
+    private final LayoutInflater inflater;
+
+    public ImageAdapter(Context context, List<String> images) {
+        this.context=context;
+        this.images=images;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(inflater.inflate(R.layout.item_image, parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private final ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image);
+        }
+
+        public void bind(String data) {
+
+        }
+    }
+}
